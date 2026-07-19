c726d4de59369c77277988597336a6a6049b4f76
gautamgit676 <gautamgit244@gmail.com>
2026-07-19 15:13:30 +0530

    add new chnage in s3 add

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=10)
     place = models.CharField(max_length=10)
+    image = models.FileField(upload_to="students/")
--- pro/settings.py
+++ pro/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'rest_framework'
+    'rest_framework',
+     "storages",
 ]
 
 MIDDLEWARE = [
@@ -89,6 +90,19 @@ DATABASES = {
     }
 }
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -145,8 +159,11 @@ CELERY_RESULT_BACKEND = (
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# local
+# MEDIA_URL = "/media/". 
+# MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
